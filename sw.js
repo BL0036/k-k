6623ef38b7dc566bcf0d71488f82b2ae88a5729a
Biswas Lamichhane <biswaslamichhane00036@gmail.com>
2026-08-03 16:40:10 +0545

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "kk-ledger-v10";
+const CACHE_NAME = "kk-ledger-v11";
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(["./", "./index.html"])));
   self.skipWaiting();
